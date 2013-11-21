5323d2f2f898e708437e5ce123df822977878817
yale <yale@newslook.com>
2013-11-21 17:22:58 -0500

    ability to configure columns

--- lib/settings.rb
+++ lib/settings.rb
@@ -13,34 +13,51 @@ class Settings
   @@expires = Time.now - Time.now.to_i
   @@cache = {}
   @@model = nil
-  
+  @@columns = {
+    entity: :entity,
+    attr: :attr,
+    value: :value,
+    json: :json
+  }
+
   class << self
-    def use_model model
-      @@model = model
-    end
-    
-    def fetch!
-      @@expires = Time.now + 300
-      @@cache = {}
-      @@model.all.each do |setting| 
-        entity, attr = setting.entity.to_sym, setting.attr.to_sym
-        @@cache[entity] ||= {}
-        @@cache[entity][attr] = format_value_for(setting)
-      end
-    end
-    
     def [] key
       fetch! if Time.now > @@expires
       @@cache[key]
     end
-    
-  private
+
+    def fetch!
+      @@expires = Time.now + 300
+      @@cache = {}
+      @@model.all.each do |setting| 
+        entity, attr, _ = raw_eav(setting)
+        @@cache[entity] ||= {}
+        @@cache[entity][attr] = format_value_for(setting)
+      end
+    end
+
+    def use_model model, opts={}
+      @@model = model
+      @@columns.merge! opts[:columns] if opts[:columns]
+    end
+
+    private
+
+    def raw_eav setting
+      [
+        setting.send(@@columns[:entity]),
+        setting.send(@@columns[:attr]),
+        setting.send(@@columns[:value])
+      ]
+    end
 
     def format_value_for setting
       if setting.json
         return JSON.parse(setting.json, symbolize_names: true)
       end
-      entity, attr, value = setting.entity, setting.attr, setting.value
+
+      entity, attr, value = raw_eav(setting)
+      
       # Coerce numeric values
       if value.is_numeric?
         value = Float(value)
@@ -48,7 +65,7 @@ class Settings
           value = value.to_i
         end
       end
-      
+
       # Are we dealing with a boolean?
       attr[-1] == "?" ? value == 1 : value
     end
